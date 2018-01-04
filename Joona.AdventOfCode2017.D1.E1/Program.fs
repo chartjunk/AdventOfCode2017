b12c43f10452e515269770c82452911c838a5488
Joona Luoma <joona.luoma@solteq.com>
2018-01-04 22:19:10 +0200

    Initial commit and D1.E1

---
+++ Joona.AdventOfCode2017.D1.E1/Program.fs
@@ -0,0 +1,4 @@
+﻿open System
+
+let rec sumDubs (l:List<Char>) = match l with | o when o.Tail.IsEmpty -> 0 | h::t when h.Equals t.Head -> sumDubs t + int h - int '0' | _::t -> sumDubs t
+[<EntryPoint>] let main argv = (argv.[0] |> Seq.last)::[for c in argv.[0] -> c] |> sumDubs |> printfn "%i"; Console.ReadKey() |> ignore; 0
