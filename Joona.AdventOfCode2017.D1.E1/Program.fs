93a5895ad85e0016f4095a319a4233d400a77dab
Joona Luoma <joona.luoma@solteq.com>
2018-01-04 22:20:38 +0200

    Fix formatting

--- Joona.AdventOfCode2017.D1.E1/Program.fs
+++ Joona.AdventOfCode2017.D1.E1/Program.fs
@@ -1,4 +1,15 @@
 ﻿open System
 
-let rec sumDubs (l:List<Char>) = match l with | o when o.Tail.IsEmpty -> 0 | h::t when h.Equals t.Head -> sumDubs t + int h - int '0' | _::t -> sumDubs t
-[<EntryPoint>] let main argv = (argv.[0] |> Seq.last)::[for c in argv.[0] -> c] |> sumDubs |> printfn "%i"; Console.ReadKey() |> ignore; 0
+let rec sumDubs (l:List<Char>) = 
+    match l with 
+    | o when o.Tail.IsEmpty -> 0 
+    | h::t when h.Equals t.Head -> sumDubs t + int h - int '0' 
+    | _::t -> sumDubs t
+
+[<EntryPoint>] 
+let main argv = 
+    (argv.[0] |> Seq.last)::[for c in argv.[0] -> c] 
+    |> sumDubs 
+    |> printfn "%i"
+    Console.ReadKey() |> ignore
+    0
