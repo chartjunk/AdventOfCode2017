cbd5b38edd5fb14e8b702eacc3177ac9b18ffa31
Joona Luoma <joona.luoma@solteq.com>
2018-01-04 22:35:01 +0200

    Remove unnecessary Equals

--- Joona.AdventOfCode2017.D1.E1/Program.fs
+++ Joona.AdventOfCode2017.D1.E1/Program.fs
@@ -3,7 +3,7 @@
 let rec sumDubs (l:List<Char>) = 
     match l with 
     | _::[] -> 0 
-    | h::t when h.Equals t.Head -> sumDubs t + int h - int '0' 
+    | h::t when h = t.Head -> sumDubs t + int h - int '0' 
     | _::t -> sumDubs t
 
 [<EntryPoint>] 
