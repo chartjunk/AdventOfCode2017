a6fd7837bbdf2661baca9fe7668b6d2331bd1c72
Joona Luoma <joona.luoma@solteq.com>
2018-02-17 18:49:19 +0200

    Solve day 6 e2

--- Joona.AdventOfCode2017.D6.E1/Program.fs
+++ Joona.AdventOfCode2017.D6.E1/Program.fs
@@ -1,34 +1,9 @@
 ﻿open System
 open ClipboardUtils
 open StringUtils
-
-let flip f a b = f b a
-
-let distributeUntilCollision initialMemory =
-    
-    let bCountf = initialMemory |> Array.length |> float    
-    
-    let distribute memory =
-        let max = memory |> Array.max
-        let maxIx = memory |> Array.findIndex ((=)max)
-        let distributionForBank b = 
-            let maxIxf = float maxIx
-            let maxf = float max
-            let bf = float b
-            floor((maxf + bCountf - (bCountf + (bf-maxIxf-1.)) % bCountf - 1.)/bCountf) |> int
-        memory |> Array.mapi(fun ix v -> 
-            match distributionForBank ix with
-            | d when ix = maxIx -> d
-            | d -> d + v)
-
-    let rec cycle (history : Set<Array>) =
-        function
-        | memory when memory |> history.Contains -> history.Count
-        | memory -> memory |> history.Add |> flip cycle (distribute memory)
-    
-    cycle Set.empty initialMemory
+open MemoryDistribution
 
 [<EntryPoint; STAThread>]
 let main argv =
-    splitClean tab >> Array.map int >> distributeUntilCollision >> string |> rotateClipboard
+    splitClean tab >> Array.map int >> distributeUntilCollision >> snd >> string |> rotateClipboard
     0
---
+++ Joona.AdventOfCode2017.D6.E2/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+open ClipboardUtils
+open StringUtils
+open MemoryDistribution
+
+[<EntryPoint; STAThread>]
+let main argv =    
+    splitClean tab >> Array.map int
+    
+    // Get colliding memory pattern
+    >> distributeUntilCollision >> fst
+    // Get loop size
+    >> distributeUntilCollision >> snd 
+
+    >> string |> rotateClipboard
+    0
