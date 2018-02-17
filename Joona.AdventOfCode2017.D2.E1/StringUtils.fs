3eaf9fc0ed97389f17a8d7db4e26867ca342b2d4
Joona Luoma <joona.luoma@solteq.com>
2018-02-17 15:54:26 +0200

    Solve day 5 e1

--- Joona.AdventOfCode2017.D2.E1/StringUtils.fs
+++ Joona.AdventOfCode2017.D2.E1/StringUtils.fs
@@ -1,5 +1,6 @@
 ﻿module StringUtils
     open System
     let newline = Environment.NewLine
+    let tab = '\t' |> string
     let split (splitter:string) (options:StringSplitOptions) (text:string) = text.Split([|splitter|], options)
     let splitClean s = split s StringSplitOptions.RemoveEmptyEntries
