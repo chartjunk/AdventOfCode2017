7be5cd2fa7c782231180d056070a90bd2365e947
Joona Luoma <joona.luoma@solteq.com>
2018-02-21 23:18:15 +0200

    Solve day 7 e1

--- Joona.AdventOfCode2017.D2.E1/StringUtils.fs
+++ Joona.AdventOfCode2017.D2.E1/StringUtils.fs
@@ -2,5 +2,7 @@
     open System
     let newline = Environment.NewLine
     let tab = '\t' |> string
+    let space = " "
     let split (splitter:string) (options:StringSplitOptions) (text:string) = text.Split([|splitter|], options)
     let splitClean s = split s StringSplitOptions.RemoveEmptyEntries
+    let splitCleanList s = splitClean s >> List.ofArray
---
+++ Joona.AdventOfCode2017.D7.E1/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open ClipboardUtils
+open StringUtils
+
+let flip f a b = f b a
+let getOwnName = splitClean space >> Array.head
+let getUpperNames = 
+    splitCleanList " -> "
+    >> (function
+    | _::x::_ -> x |> splitCleanList ", "
+    | _ -> List.Empty)
+
+[<EntryPoint; STAThread>]
+let main argv =
+    splitCleanList newline
+    >> (fun lines -> 
+        let upperNames = lines |> List.map getUpperNames |> List.collect id |> set
+        lines |> Seq.map getOwnName |> Seq.find (flip Set.contains upperNames >> not))
+    |> rotateClipboard
+    0
