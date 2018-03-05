d491744794c142ea7c7534320b909c2f4436e5e5
Joona Luoma <joona.luoma@solteq.com>
2018-03-05 21:11:03 +0200

    Solve day 9 e1

---
+++ Joona.AdventOfCode2017.D9.E1/Program.fs
@@ -0,0 +1,40 @@
+﻿open System
+open ClipboardUtils
+open FunctionalUtils
+
+let cancel =
+    let rec cancelAcc acc =
+        function
+        | [] -> acc
+        | '!'::_::cs -> cancelAcc acc cs
+        | c::cs -> cancelAcc (acc@[c]) cs
+    cancelAcc []
+
+let piece =
+    let rec pieceAcc acc =
+        function
+        | [] -> acc
+        | '<'::cs -> cs |> List.skipWhile((<>)'>') |> List.skip 1 |> pieceAcc acc
+        | c::cs -> pieceAcc (acc@[c]) cs
+    pieceAcc []
+
+let group =
+    let rec groupAcc acc level =
+        function
+        | [] -> acc
+        | '{'::cs -> groupAcc (acc+level+1) (level+1) cs
+        | '}'::cs -> groupAcc acc (level-1) cs
+    flip groupAcc 0
+
+[<EntryPoint;STAThread>]
+let main argv =
+    Seq.toList
+    >> List.filter((<>)'\r')
+    >> List.filter((<>)'\n')
+    >> cancel 
+    >> piece 
+    >> List.filter((<>)',')
+    >> group 0
+    >> string
+    |> rotateClipboard
+    0
