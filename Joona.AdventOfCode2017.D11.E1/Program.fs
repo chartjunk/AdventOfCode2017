a5d471b8241d90a8511e568bc2b886d078aa2267
Joona Luoma <joona.luoma@solteq.com>
2018-04-30 12:14:22 +0300

    Solve day 11 e1

---
+++ Joona.AdventOfCode2017.D11.E1/Program.fs
@@ -0,0 +1,20 @@
+﻿open StringUtils
+open ClipboardUtils
+open System
+open FunctionalUtils
+
+[<EntryPoint;STAThread>]
+let main _ =
+    trim >> splitClean ","
+    >> Seq.map (function
+    | "n"  -> [0;-1;1]
+    | "s"  -> [0;1;-1]
+    | "nw" -> [1;-1;0]
+    | "se" -> [-1;1;0]
+    | "ne" -> [-1;0;1]
+    | "sw" -> [1;0;-1])
+    >> Seq.reduce (List.map2 (+))
+    >> Seq.sumBy abs >> flip (/)2
+    >> string
+    |> rotateClipboard
+    0
--- Joona.AdventOfCode2017.D2.E1/StringUtils.fs
+++ Joona.AdventOfCode2017.D2.E1/StringUtils.fs
@@ -3,7 +3,8 @@
     let newline = Environment.NewLine
     let tab = '\t' |> string
     let space = " "
+    let clear (s:String) (c:String) = s.Replace(c, "")
+    let trim (s:String) = s.Trim()
     let split (splitter:string) (options:StringSplitOptions) (text:string) = text.Split([|splitter|], options)
     let splitClean s = split s StringSplitOptions.RemoveEmptyEntries
     let splitCleanList s = splitClean s >> List.ofArray
-    let clear (s:String) (c:String) = s.Replace(c, "")
