b1d3210bcddbc82849d693cf72dc782528295a34
Joona Luoma <joona.luoma@solteq.com>
2018-05-01 14:52:41 +0300

    Solve day 12 e2

---
+++ Joona.AdventOfCode2017.D12.E2/Program.fs
@@ -0,0 +1,20 @@
+﻿open ClipboardUtils
+open System
+open Pipe
+open FunctionalUtils
+
+[<EntryPoint;STAThread>]
+let main _ =
+    stringToList
+    >> fw Map.ofList
+    >> fun (l, m) -> 
+        l
+        |> Seq.map fst
+        |> Seq.fold (fun s id -> 
+            match s with
+            | (union,_) when union |> Set.contains id -> s
+            | (union,c) -> union |> Set.union (getAreaForId id m), c+1) (set[], 0)
+    >> snd
+    >> string
+    |> rotateClipboard
+    0
