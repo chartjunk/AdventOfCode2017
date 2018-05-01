d7196682fbe355a924c313f0496c6ffc0d77a639
Joona Luoma <joona.luoma@solteq.com>
2018-05-01 15:09:47 +0300

    Refactor

--- Joona.AdventOfCode2017.D12.E2/Program.fs
+++ Joona.AdventOfCode2017.D12.E2/Program.fs
@@ -10,10 +10,10 @@ let main _ =
     >> fun (l, m) -> 
         l
         |> Seq.map fst
-        |> Seq.fold (fun s id -> 
-            match s with            
+        |> Seq.fold (flip (fun id -> 
+            function       
             | (union, c) when union |> Set.contains id |> not -> union |> Set.union (getAreaForId id m), c+1
-            | _ -> s) (set[], 0)
+            | s -> s)) (set[], 0)
     >> snd
     >> string
     |> rotateClipboard
